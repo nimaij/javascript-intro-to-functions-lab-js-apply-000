8f9962c84b82dd1638e4c63461f4fad2ddab8b98
Jiamin Lim <jiaminlim93@gmail.com>
2018-11-02 16:22:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,11 +15,9 @@ function logWhisper(string){
 }
 
 function sayHiToGrandma(string){
-  if string=string.toLowerCase(){
+  if (string == string.toLowerCase()) {
     return "I can't hear you!"
-  } else if string=string.toUpperCase(){
+  } else if (string == string.toUpperCase()){
     "YES INDEED!"
-  } else{
-    
   }
 }
